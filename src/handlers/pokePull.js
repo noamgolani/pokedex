c41c5284acb4e68bcd70336d8f2e72d61beaf833
noamgolani <noam.golani@gmail.com>
2021-10-14 06:31:37 -0700

    added the fetching

--- src/handlers/pokePull.js
+++
@@ -1,7 +0,0 @@
-import $ from "jquery";
-import { pokeClick } from "./pokemon";
-
-$("#poke-cont").on("click", function (event) {
-  if (!event.target.classList.contains("poke")) return;
-  console.log(event.target.id);
-});
--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 import "./styles/styles.css";
 
-import "./handlers/pokePull";
-
 import axios from "axios";
 import $ from "jquery";
-import Pokemon, { pokemonHtml } from "./pokemon";
 
 const URI = "https://pokeapi.co/api/v2";
 
-const pokes = [];
-
 $("#search").on("click", () => {
   axios.get(`${URI}/pokemon/${$("#searchValue").val()}`).then((response) => {
-    const newPoke = new Pokemon(response.data);
-    pokes.push(newPoke);
-    renderPokes();
+    const { id, name, sprites, weight, height } = response.data;
+    $("#poke-cont").append(`
+      <div class="poke" id="${id}">
+        <div class="info">
+          <h2>${name}</h2>
+          <p>
+            Width: ${weight} <br>
+            Height: ${height}
+          </p>
+        </div>
+        <img src="${sprites.front_default}" id="frontS">
+        <img src="${sprites.back_default}" id="backS">
+      </div>
+    `);
   });
 });
-
-function renderPokes() {
-  for (const poke of pokes) {
-    $("#poke-cont").append(pokemonHtml(poke.getData()));
-  }
-}
