a5d8a7f4cefe35148c9e338e0958bd506a13ac34
noamgolani <noam.golani@gmail.com>
2021-10-16 15:14:50 -0700

    auto complete shows on page

--- src/handlers.js
+++ src/handlers.js
@@ -2,6 +2,7 @@ import $ from "jquery";
 import { getPokesIds, getPokesNames, addPoke } from "./pokemon";
 import { showError } from "./error";
 import { getPoke } from "./pokeapi";
+import names from "./names";
 
 export function handleTypeClick(event) {
   const typeName = event.target.dataset.name;
@@ -31,4 +32,22 @@ export function handleSearchClick(event) {
       .catch((err) => {
         showError(`API Error: ${err}`);
       });
+}
+
+export function handleSearchChange() {
+  const searchInput = $("#searchValue");
+  const suggestions =
+    searchInput.val().trim() === ""
+      ? []
+      : names.filter((names) =>
+          names.toLowerCase().includes(searchInput.val().trim().toLowerCase())
+        );
+
+  const autoC = $(".autocomplete");
+  autoC.empty();
+  suggestions.slice(0, 10).forEach((sug) => {
+    autoC.append(`
+      <li class="list-group-item">${sug}</li>
+    `);
+  });
 }
